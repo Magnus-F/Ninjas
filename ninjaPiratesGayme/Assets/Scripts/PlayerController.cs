1b240b4376c424fd48f6dfe775298ff387c0d278
wtw1820 <wtw1820@IGM2435-22>
2020-02-14 10:48:58 -0500

    worked on playercontroller

--- ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
+++ ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Ray ray;
+    RaycastHit hit;
+
+    public GameObject block;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetKey(KeyCode.Mouse0))
+            {
+                GameObject obj = Instantiate(block, new Vector3(hit.point.x, hit.point.y, 0), Quaternion.identity) as GameObject;
+            }
+        }
     }
 }
