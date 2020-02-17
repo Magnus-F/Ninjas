32ee40753f7fd68220119348c832dbb39f6c3ad2
wtw1820 <wtw1820@IGM2435-22>
2020-02-17 10:55:54 -0500

    bkahdlif

--- ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
+++ ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
@@ -13,6 +13,7 @@ public class PlayerController : MonoBehaviour
 
     GameObject selectedBlock;
     GameObject ghostBlock;
+    GameObject[] blocks;
 
     // Start is called before the first frame update
     void Start()
@@ -20,14 +21,23 @@ public class PlayerController : MonoBehaviour
         selectedBlock = OBlock;
         ghostBlock = Instantiate(selectedBlock, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
         Destroy(ghostBlock.GetComponent<Rigidbody2D>());
-        Destroy(ghostBlock.GetComponent<BoxCollider2D>());
+        ghostBlock.GetComponent<BoxCollider2D>().enabled = false;
         ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
-        
+        ghostBlock.tag = "Untagged";
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        bool placable = true;
+        if (Input.GetMouseButtonDown(0))
+        {
+            blocks = GameObject.FindGameObjectsWithTag("Block");
+        }
+
+
+
         ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
         if (Input.GetMouseButton(0))
         {
@@ -35,7 +45,9 @@ public class PlayerController : MonoBehaviour
             Vector3 ghostpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             ghostpos.z = 0;
             ghostBlock.transform.position = ghostpos;
+
             
+
         }
         
 
@@ -44,18 +56,20 @@ public class PlayerController : MonoBehaviour
             selectedBlock = OBlock;
             Destroy(ghostBlock);
             ghostBlock = Instantiate(selectedBlock, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
-            Destroy(ghostBlock.GetComponent<Rigidbody2D>());
+            ghostBlock.GetComponent<BoxCollider2D>().enabled = false;
             Destroy(ghostBlock.GetComponent<BoxCollider2D>());
             ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
+            ghostBlock.tag = "Untagged";
         }
         if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             selectedBlock = IBlock;
             Destroy(ghostBlock);
             ghostBlock = Instantiate(selectedBlock, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
-            Destroy(ghostBlock.GetComponent<Rigidbody2D>());
+            ghostBlock.GetComponent<BoxCollider2D>().enabled = false;
             Destroy(ghostBlock.GetComponent<BoxCollider2D>());
             ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
+            ghostBlock.tag = "Untagged";
         }
 
 
@@ -63,7 +77,7 @@ public class PlayerController : MonoBehaviour
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
-            Instantiate(selectedBlock, pos, Quaternion.identity);
+            if(placable)Instantiate(selectedBlock, pos, Quaternion.identity);
         }
     }
 }
