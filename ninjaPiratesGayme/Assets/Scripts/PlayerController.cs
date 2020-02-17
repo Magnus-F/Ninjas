aa6f158832249069dd5a146efe7f0dc3aad5701b
wtw1820 <wtw1820@IGM2435-22>
2020-02-17 10:31:50 -0500

    you can switch between O and I blocks. You can also hold the mouse button down to get a preview

--- ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
+++ ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
@@ -8,23 +8,62 @@ public class PlayerController : MonoBehaviour
     Ray ray;
     RaycastHit hit;
 
-    public GameObject block;
+    public GameObject OBlock;
+    public GameObject IBlock;
+
+    GameObject selectedBlock;
+    GameObject ghostBlock;
 
     // Start is called before the first frame update
     void Start()
     {
+        selectedBlock = OBlock;
+        ghostBlock = Instantiate(selectedBlock, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+        Destroy(ghostBlock.GetComponent<Rigidbody2D>());
+        Destroy(ghostBlock.GetComponent<BoxCollider2D>());
+        ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
+        if (Input.GetMouseButton(0))
+        {
+            ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .25f);
+            Vector3 ghostpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            ghostpos.z = 0;
+            ghostBlock.transform.position = ghostpos;
+            
+        }
+        
 
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKeyDown(KeyCode.Keypad0))
+        {
+            selectedBlock = OBlock;
+            Destroy(ghostBlock);
+            ghostBlock = Instantiate(selectedBlock, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            Destroy(ghostBlock.GetComponent<Rigidbody2D>());
+            Destroy(ghostBlock.GetComponent<BoxCollider2D>());
+            ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
+        }
+        if (Input.GetKeyDown(KeyCode.Keypad1))
+        {
+            selectedBlock = IBlock;
+            Destroy(ghostBlock);
+            ghostBlock = Instantiate(selectedBlock, Camera.main.ScreenToWorldPoint(Input.mousePosition), Quaternion.identity);
+            Destroy(ghostBlock.GetComponent<Rigidbody2D>());
+            Destroy(ghostBlock.GetComponent<BoxCollider2D>());
+            ghostBlock.GetComponent<SpriteRenderer>().color = new Color(1f, 1f, 1f, .0f);
+        }
+
+
+        if (Input.GetMouseButtonUp(0))
         {
             Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             pos.z = 0;
-            Instantiate(block, pos, Quaternion.identity);
+            Instantiate(selectedBlock, pos, Quaternion.identity);
         }
     }
 }
