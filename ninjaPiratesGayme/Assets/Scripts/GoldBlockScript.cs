c09a71fc9774f2dff46ddc0fd084ed4a79c20bfb
Jordan Hemmers <jhemmers@hotmail.com>
2020-02-21 05:32:10 -0500

    more work on shooting ninja stars

---
+++ ninjaPiratesGayme/Assets/Scripts/GoldBlockScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoldBlockScript : MonoBehaviour
+{
+    public GameObject ninjaStar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cursorInWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+            Vector2 direction = target - myPos;
+            direction.Normalize();
+            Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+            GameObject projectile = (GameObject)Instantiate(ninjaStar, myPos, rotation);
+            projectile.GetComponent<Rigidbody2D>().velocity = direction * 5f;
+        }
+    }
+}
