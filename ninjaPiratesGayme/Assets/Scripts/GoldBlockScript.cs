a2e012da38abdb0a1b062fd6d6306c13bd476a3e
Jordan Hemmers <jhemmers@hotmail.com>
2020-02-27 12:00:07 -0500

    fixed ninja star UI button glitch

--- ninjaPiratesGayme/Assets/Scripts/GoldBlockScript.cs
+++ ninjaPiratesGayme/Assets/Scripts/GoldBlockScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GoldBlockScript : MonoBehaviour
 {
@@ -21,14 +22,17 @@ public class GoldBlockScript : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0) && myLevelManager.ninjaStarCount > 0)
         {
-            Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
-            Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
-            Vector2 direction = target - myPos;
-            direction.Normalize();
-            Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-            GameObject projectile = (GameObject)Instantiate(ninjaStar, myPos, rotation);
-            projectile.GetComponent<Rigidbody2D>().velocity = direction * 10f;
-            myLevelManager.ninjaStarCount -= 1;
+            if (!EventSystem.current.IsPointerOverGameObject())
+            {
+                Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+                Vector2 myPos = new Vector2(transform.position.x, transform.position.y);
+                Vector2 direction = target - myPos;
+                direction.Normalize();
+                Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+                GameObject projectile = (GameObject)Instantiate(ninjaStar, myPos, rotation);
+                projectile.GetComponent<Rigidbody2D>().velocity = direction * 10f;
+                myLevelManager.ninjaStarCount -= 1;
+            }
         }
     }
 }
--- ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
+++ ninjaPiratesGayme/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     Ray ray;
     RaycastHit hit;
 
