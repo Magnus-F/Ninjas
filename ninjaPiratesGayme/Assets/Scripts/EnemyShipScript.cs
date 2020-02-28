c8178594d312ac2c3d3200661c554f00b75bc5de
Jordan Hemmers <jhemmers@hotmail.com>
2020-02-28 07:09:04 -0500

    fixed shooting glitch

--- ninjaPiratesGayme/Assets/Scripts/EnemyShipScript.cs
+++ ninjaPiratesGayme/Assets/Scripts/EnemyShipScript.cs
@@ -66,4 +66,16 @@ public class EnemyShipScript : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    void OnMouseDown()
+    {
+        Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+        Vector2 myPos = new Vector2(goldBlock.transform.position.x, goldBlock.transform.position.y);
+        Vector2 direction = target - myPos;
+        direction.Normalize();
+        Quaternion rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
+        GameObject projectile = (GameObject)Instantiate(myLevelManager.ninjaStarToUse, myPos, rotation);
+        projectile.GetComponent<Rigidbody2D>().velocity = direction * 10f;
+        myLevelManager.ninjaStarCount -= 1;
+    }
 }
