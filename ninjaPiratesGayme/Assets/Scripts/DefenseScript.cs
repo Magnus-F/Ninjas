b88a8821e75b806481af134cf907c774677aeb80
Pancakes42 <36553713+Pancakes42@users.noreply.github.com>
2020-03-02 19:33:40 -0500

    I can't test it rn but this is what I think I did. Ninja stars shoot out of both sides now, and aim towards the nearest pirate within range.

--- ninjaPiratesGayme/Assets/Scripts/DefenseScript.cs
+++ ninjaPiratesGayme/Assets/Scripts/DefenseScript.cs
@@ -9,6 +9,8 @@ public class DefenseScript : MonoBehaviour
     public float cannonTimer;
     public LevelManager myLevelManager;
     public bool canShoot;
+    public float projectilespeed;
+    float verticalrange;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +18,10 @@ public class DefenseScript : MonoBehaviour
         cannonTimer = 0;
         myLevelManager = FindObjectOfType<LevelManager>();
         canShoot = false;
+
+
+        verticalrange = 2.0f; // decides the verticle range of the turret's aiming
+        projectilespeed = 0.5f; //decides the speed of the ninja star
     }
 
     // Update is called once per frame
@@ -27,8 +33,64 @@ public class DefenseScript : MonoBehaviour
         }
         else if(cannonTimer > 2 && canShoot)//myLevelManager.ninjaStarCount > 0)
         {
-            GameObject projectile = Instantiate(myLevelManager.ninjaStarToUse, new Vector3(this.transform.position.x-2, this.transform.position.y, this.transform.position.z), this.transform.rotation);
-            projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(-10f, 0);
+            
+
+            GameObject[] pirates = GameObject.FindGameObjectsWithTag("Enemy");
+
+            GameObject closestrightpirate = new GameObject();
+            GameObject closestleftpirate = new GameObject();
+
+            float crpd = 10000.0f;
+            float clpd = 10000.0f;
+
+            
+
+            foreach(GameObject e in pirates)
+            {
+                if (e.transform.position.y < transform.position.y + verticalrange && e.transform.position.y > transform.position.y - verticalrange)
+                {
+                    if (e.transform.position.x > transform.position.x)
+                    {
+                        if(Vector2.Distance(e.transform.position,transform.position) < crpd)
+                        {
+                            crpd = Vector2.Distance(e.transform.position, transform.position);
+                            closestrightpirate = e;
+                        }
+                        
+                    }
+                    else
+                    {
+                        if (Vector2.Distance(e.transform.position, transform.position) < clpd)
+                        {
+                            clpd = Vector2.Distance(e.transform.position, transform.position);
+                            closestleftpirate = e;
+
+                        }
+                    }
+                }
+                
+            }
+
+            if (closestrightpirate.tag == "Enemy") {
+                GameObject projectile2 = Instantiate(myLevelManager.ninjaStarToUse, new Vector3(this.transform.position.x + 2, this.transform.position.y, this.transform.position.z), this.transform.rotation);
+                Vector3 spd = closestleftpirate.transform.position - transform.position;
+                spd = Vector3.Normalize(spd) * projectilespeed;
+                projectile2.GetComponent<Rigidbody2D>().velocity = spd;
+            }
+
+            if(closestleftpirate.tag == "Enemy")
+            {
+                GameObject projectile1 = Instantiate(myLevelManager.ninjaStarToUse, new Vector3(this.transform.position.x - 2, this.transform.position.y, this.transform.position.z), this.transform.rotation);
+                Vector3 spd = closestrightpirate.transform.position - transform.position;
+                spd = Vector3.Normalize(spd) * projectilespeed;
+                projectile1.GetComponent<Rigidbody2D>().velocity = spd;
+            }
+
+
+            
+
+
+
             //Vector2 target = Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
             //Vector2 myPos = new Vector2(theCannon.transform.position.x, theCannon.transform.position.y);
             //Vector2 direction = target - myPos;
